24c082744e4cb089b1af8944f7e627f9b929d9ca
James Hizon <52821013+JamesHizon@users.noreply.github.com>
2021-04-06 13:28:41 -0700

    Updated Python script

--- Step 3 - Data Collection/Project_Data_Collection.py
+++ Step 3 - Data Collection/Project_Data_Collection.py
@@ -9,22 +9,35 @@
 
 import requests
 from bs4 import BeautifulSoup
+from time import time
 
+# Extract data from HTML webpage
 URL = 'http://data.gdeltproject.org/gkg/index.html'
 page = requests.get(URL)
 
+# Create BeautifulSoup object to parse through HTML document
 soup = BeautifulSoup(page.content, 'html.parser')
 
+# Base url for file downloading
 base_url = 'http://data.gdeltproject.org/gkg/'
 
+# Iterate to obtain all 14 files we want to download based on "a" and "href" tags
 for link in soup.find_all('a')[2:16]:
+    # Count time it takes to download each link
+    print("Start Execution : ")
+    time.ctime()  # Initialize time object to return time string
     file_link = link.get('href')
     if link.has_attr('href'):
         file = link.attrs['href']
+        # Create "download_url" object from list of file names (file) and base_url
         download_url = f"{base_url}{file}"
+        # Introduce time object inside context manager to track file download time
         with open(f"{file}", 'wb') as file:
             response = requests.get(download_url)
             file.write(response.content)
+            # Stop execution time
+            print("Stop Execution : ", end="")
+            print(time.ctime())
 
 
 
