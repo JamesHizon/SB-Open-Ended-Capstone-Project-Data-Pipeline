9e46ffbde7babbdaded60d1ccf5e56b7bc3b776f
jameshizon <jamesrhizon@gmail.com>
2021-04-10 12:58:45 -0700

    Push Python file.

--- Step 3 - Data Collection/Project_Data_Collection.py
+++ Step 3 - Data Collection/Project_Data_Collection.py
@@ -37,19 +37,18 @@ for link in soup.find_all('a')[2:16]: # Do we still need to use array slicing?
             for date in date_list:
                 date.strftime("%Y%d%m")
             logging.info('Will proceed to downloading the past week of files.')
-            # 2) Loop through values that have the date within file name.
+            # Loop through values that have the date within file name.
             for date in file:
                 download_url = f"{base_url}{file}"
                 with open(f"{file}", "wb") as file:
                     response = requests.get(download_url)
                     file.write(response.content)
                     logging.info('Files have been downloaded as ZIP.')
-                    # Can try to read new file and convert to ZIP file (automate file unzipping):
+                    # Convert to ZIP file (automate file unzipping):
                     with zipfile.ZipFile('/Users/jameshizon/Downloads/Open_Ended_Capstone_Project', 'r') as zip_ref:
                         zip_ref.extractall('/Users/jameshizon/Downloads/Open_Ended_Capstone_Project')
                         logging.info('Files have been unzipped.')
     except Exception as e:
-        # Logging message
         logging.error(f"Error was {e}")
 
 
