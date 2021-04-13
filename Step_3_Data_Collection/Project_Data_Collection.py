21397e2e168e5c7ca6d51f2bbf67d82e7175bb0b
jameshizon <jamesrhizon@gmail.com>
2021-04-12 19:11:13 -0700

    Push Python file.

--- Step_3_Data_Collection/Project_Data_Collection.py
+++ Step_3_Data_Collection/Project_Data_Collection.py
@@ -5,8 +5,8 @@ import requests
 from bs4 import BeautifulSoup
 import datetime
 import logging
-# import zipfile
-import pandas as pd
+import zipfile
+import os
 
 # Control logging levels:
 logging.basicConfig(level=logging.INFO)
@@ -21,39 +21,38 @@ soup = BeautifulSoup(page.content, 'html.parser')
 # Base url for file downloading
 base_url = 'http://data.gdeltproject.org/gkg/'
 
+# Current Directory
+current_directory = os.getcwd()
+
 # Iterate to obtain all 14 files we want to download based on "a" and "href" tags and write to file
 
 for link in soup.find_all('a')[2:16]: # Do we still need to use array slicing?
     try:
         if link.has_attr('href'):
+            # Obtain file name as string object:
             file = link.attrs['href']
             # Create a list of dates to control which files to download:
             today = datetime.date.today()
             # List of dates:
             date_list = [today]
-            # Add other dates
+            # Add other dates (within 1 week):
             for i in range(1, 8):
                 new_day = today - datetime.timedelta(days=i)
                 date_list.append(new_day)
-            # Convert dates to string
+            # Convert dates to string to filter file name
             for date in date_list:
-                date.strftime("%Y%d%m")
-            logging.info('Will proceed to downloading the past week of files.')
-            # Loop through values that have the date within file name.
-            for date in file:
-                download_url = f"{base_url}{file}"
-                with open(f"{file}", "wb") as zip_file:
-                    response = requests.get(download_url)
-                    zip_file.write(response.content)
-                    logging.info('Files have been downloaded as ZIP.')
-                    # Unzip to CSV file
-                    # with open(f"{file.rstrip('.zip')}", "wb") as csv_file:
-                    #     csv_file.write(zip_file.content)  # Write file content to csv_file
-                    with open(f"{file.rstrip('.zip')}") as csv_file:
-                        csv_df = pd.read_csv(csv_file, header=0, delimiter="\t")
-                        # Apply compression to csv:
-                        compression_opts = dict(method='zip', archive_name=csv_file)
-                        csv_df.to_csv(f"{file}", index=False, compression=compression_opts)
+                date = date.strftime("%Y%m%d")
+                if date in file:
+                    download_url = f"{base_url}{file}"
+                    with open(f"{file}", "wb") as zip_file:
+                        response = requests.get(download_url)
+                        zip_file.write(response.content)
+                    logging.info("Zip file has been downloaded.")
+                    # Use after creating zipfile to unpack as a CSV
+                    with zipfile.ZipFile(f"{file}", "r") as zip_ref:
+                        zip_ref.extractall(current_directory)
+                    logging.info("File has been downloaded and unpacked as CSV.")
+        logging.info("All files have been downloaded.")
     except Exception as e:
         logging.error(f"Error was {e}")
 
